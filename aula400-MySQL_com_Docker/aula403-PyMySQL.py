b346d769e228a9570f529c5c1b935fc99ef343f5
Luiz <lom.raiol@gmail.com>
2024-07-11 21:01:25 -0300

    atualizacao aula

--- aula400-MySQL_com_Docker/aula403-PyMySQL.py
+++ aula400-MySQL_com_Docker/aula403-PyMySQL.py
@@ -1,5 +1,5 @@
-# PyMySQL - um cliente MySQL feito em Python Puro
-# AULAS: 403, 404, 405, 406, 407, 408, 409, 410, 411
+# PyMySQL - um cliente MySQL feito em Python Puro 
+# AULAS: 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413
 # lembrando: O MariaDB é um fork do MySQL, ou seja, eles funcionam quase que de maneira similar
 # pip install pymysql
 import pymysql
@@ -99,9 +99,40 @@ with connection:
             ("Cortana", 15),
         )
         result = cursor.executemany(sql, data4)
-        print(sql)
-        print(data4)
-        print(result)
+        # print(sql)
+        # print(data4)
+        # print(result)
     connection.commit()
 
     # Lendo os valores com SELECT
+    with connection.cursor() as cursor:
+        # menor_id = input('Digite o menor id: ')
+        # maior_id = input('Digite o maior id: ')
+        menor_id = 2
+        maior_id = 4
+        coluna = 'id'
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+            'WHERE id BETWEEN %s AND %s '
+        )
+        cursor.execute(sql, (menor_id, maior_id))
+        # print(cursor.mogrify(sql, (menor_id, maior_id)))
+        # data5 = cursor.fetchone()
+        # print(data5)
+        # for row in data5:
+        #     print(row)
+        
+        data5 = cursor.fetchall() # aqui ele esgota o iterator
+
+    # CUIDADO COMO 'DELETE' SEM 'WHERE' 
+    with connection.cursor() as cursor:
+        sql = (
+            f'DELETE FROM {TABLE_NAME} '
+            'WHERE id = %s '
+            )
+        print(cursor.execute(sql, (1,)))
+        connection.commit()
+
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+        for row in cursor.fetchall():
+            print(row)
